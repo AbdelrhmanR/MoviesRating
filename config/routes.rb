2a0987098b04893666d7a58e50bd0e6081557a4f
abdelrhman <abdelrhmanelheely@gmailcom>
2019-01-22 08:39:14 +0200

    Add New and Create Actions with View Files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+	resources :plays
   get 'plays/index'
+  get 'plays/new'
+   
   root 'plays#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
